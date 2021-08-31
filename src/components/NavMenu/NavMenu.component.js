2565b834d9cc4fdbb644c3d2c92fe7e1a790d728
Simen H <simenmelo@gmail.com>
2021-08-31 10:04:09 +0200

    feat: Add onClick event for menu button

--- src/components/NavMenu/NavMenu.component.js
+++ src/components/NavMenu/NavMenu.component.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './styles.css';
 
 const NavMenu = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(prevState => {
+      return !prevState;
+    });
+  };
+
   return (
-    <div className='menu'>
-      <div className='menu-button'>
+    <div className={`menu ${showMenu ? 'menu-show' : ''}`}>
+      <div className='menu-button' onClick={toggleMenu}>
         <div className='menu-line-container'>
           <div className='menu-line' />
           <div className='menu-line' />
