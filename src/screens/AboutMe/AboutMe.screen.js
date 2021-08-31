e9161c6c3cedbe5064ffd9b2121ab8c6da13250c
Simen H <simenmelo@gmail.com>
2021-08-31 14:31:16 +0200

    style: Change AboutMe to 'Coming Soon'

--- src/screens/AboutMe/AboutMe.screen.js
+++ src/screens/AboutMe/AboutMe.screen.js
@@ -3,25 +3,7 @@ import './styles.css';
 const AboutMe = () => {
   return (
     <div className='aboutme-container'>
-      <h1>Who am I?</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at risus
-        varius, vehicula mauris vel, fringilla ligula. Nullam sed ligula augue.
-        Ut aliquet urna at consequat rutrum. Phasellus rhoncus, massa ut
-        sollicitudin tincidunt, ipsum ex commodo turpis, non euismod massa
-        ligula sit amet felis. Aliquam malesuada accumsan nulla, nec molestie
-        leo molestie eget. Etiam at justo quis enim eleifend gravida ultrices
-        non neque. Pellentesque iaculis elit vitae lorem varius, nec viverra dui
-        iaculis. Proin pretium sapien et aliquet congue.
-      </p>
-
-      <p>
-        Sed vitae velit porttitor, suscipit mi ut, malesuada nunc. Cras quis
-        dolor eu ipsum tempor mattis. Quisque vel malesuada tortor. Quisque
-        luctus turpis nec accumsan facilisis. Ut mauris mauris, bibendum sed
-        odio eu, accumsan elementum elit. Vivamus ac ante augue. In a eros erat.
-        Duis commodo aliquam pretium.
-      </p>
+      <h1>Coming Soon</h1>
     </div>
   );
 };
