92b5ce67fa172fc19bc3468917ad1d5d78b9e396
Simen H <simenmelo@gmail.com>
2021-08-31 04:02:24 +0200

    feat: Add home page content and styling

--- src/screens/Main/Main.screen.js
+++ src/screens/Main/Main.screen.js
@@ -3,6 +3,14 @@ import './styles.css';
 const Main = () => {
   return (
     <div className='page-container'>
+      <div className='menu-button'>
+        <div className='menu-line-container'>
+          <div className='menu-line' />
+          <div className='menu-line' />
+          <div className='menu-line' />
+        </div>
+      </div>
+
       <div className='page-content'>
         <div className='top-container'>
           <div className='nav-button nav-up'>
@@ -10,69 +18,26 @@ const Main = () => {
           </div>
         </div>
         <div className='center-container'>
+          <div className='nav-button nav-left'>
+            <div className='nav-arrow' />
+          </div>
           <div className='center-content'>
-            <div className='nav-button nav-left'>
-              <div className='nav-arrow' />
-            </div>
+            {/*  */}
             <div className='about-me'>
-              <p>
-                Lorem ipsum dolor sit amet, <br />
-                <br />
-                Consectetur adipiscing elit. Vivamus tincidunt est lorem,
-                consequat vehicula metus hendrerit vel. Vivamus purus velit,
-                suscipit a lobortis in, porttitor a ex. Class aptent taciti
-                sociosqu ad litora torquent per conubia nostra, per inceptos
-                himenaeos. Nulla pellentesque nunc tristique nunc congue porta.
-                Nunc cursus fermentum nibh euismod pulvinar. Cras ut cursus
-                eros, et luctus est. Etiam pulvinar at augue id tincidunt.
-                Pellentesque at pellentesque massa. Ut dapibus dui mauris, non
-                sollicitudin quam consectetur vitae. Curabitur lobortis bibendum
-                ullamcorper. Maecenas porta odio fermentum, vehicula leo ut,
-                lacinia risus. Vivamus pellentesque sagittis est, ac cursus
-                neque dictum vitae. Nullam ac quam viverra, pulvinar ligula nec,
-                vehicula arcu. Morbi euismod accumsan erat. Sed est magna,
-                fringilla eu suscipit vitae, vestibulum quis arcu. Fusce arcu
-                turpis, semper id laoreet et, luctus eu ligula. Vestibulum
-                tristique, neque quis faucibus tristique, nisl orci molestie
-                tellus, eget maximus quam sem id nisl. Integer convallis
-                tristique ex, in ullamcorper justo convallis nec. Maecenas
-                mauris tellus, pretium quis dictum ut, malesuada vel nisi. Ut
-                quis semper justo. Proin ante tortor, sagittis a pretium non,
-                consequat sed lacus. Interdum et malesuada fames ac ante ipsum
-                primis in faucibus. Orci varius natoque penatibus et magnis dis
-                parturient montes, nascetur ridiculus mus. Fusce ullamcorper
-                nisi pellentesque elementum tincidunt. Morbi egestas, erat ut
-                malesuada tempus, orci mauris ullamcorper nisi, sit amet
-                pharetra sem arcu ac lacus. Nunc pellentesque elit at sem
-                gravida, eget imperdiet tellus aliquet. Fusce vel mi eget turpis
-                viverra congue. Aliquam semper hendrerit pharetra. Duis quis
-                efficitur nibh. In malesuada nisl ipsum, sit amet dictum ipsum
-                varius mattis. Suspendisse dictum mi vel consequat egestas.
-                Integer hendrerit pellentesque arcu id interdum. In tempus
-                ornare finibus. Morbi in sodales dui. Integer vulputate, mauris
-                ut tempus dictum, mi justo rhoncus nunc, non congue metus metus
-                ut ante. Mauris commodo quam vel facilisis interdum. Sed urna
-                tortor, dapibus sed purus at, pellentesque viverra tellus.
-                Aliquam lacinia semper neque, ac molestie massa pretium non.
-                Phasellus iaculis mauris et dapibus hendrerit. In nec urna in
-                nibh interdum accumsan laoreet convallis enim. Maecenas
-                ullamcorper placerat odio. Maecenas quis arcu quis sem rutrum
-                aliquet. Vivamus porta elit eu ligula iaculis, sed fermentum est
-                luctus. Nulla id risus sit amet dolor porttitor facilisis.
-                Praesent lorem tortor, pharetra sit amet lobortis eget, finibus
-                eu ipsum. Donec et laoreet massa. Duis fermentum sodales risus
-                ut commodo. Nullam cursus aliquet auctor. Nam tincidunt, mi
-                vitae viverra sollicitudin, arcu lacus ornare eros, et luctus
-                augue nunc nec mauris. Nunc laoreet, sapien pulvinar ultrices
-                vehicula, leo est lacinia diam, cursus imperdiet neque tortor
-                nec turpis. Nunc commodo diam neque, ac volutpat ligula
-                porttitor id. Vestibulum aliquam lacinia ex, et blandit ipsum
-                vehicula non. Praesent eleifend a arcu eget faucibus.
-              </p>
+              <h1>Hello, I am Simen</h1>
+              <h2>Full-Stack Web Developer</h2>
+              <div className='line-seperator' />
+              <p>Welcome to my portfolio website</p>
             </div>
-            <div className='nav-button nav-right'>
-              <div className='nav-arrow' />
+            <div className='some-links'>
+              <div className='link-icon' />
+              <div className='link-icon' />
+              <div className='link-icon' />
             </div>
+            {/*  */}
+          </div>
+          <div className='nav-button nav-right'>
+            <div className='nav-arrow' />
           </div>
         </div>
         <div className='bottom-container'>
