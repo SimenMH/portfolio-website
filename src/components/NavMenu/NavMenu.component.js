4ae07912f1d69a70672e436e735c5ced682c8dac
Simen H <simenmelo@gmail.com>
2021-08-31 09:56:16 +0200

    feat: Add nav menu list

--- src/components/NavMenu/NavMenu.component.js
+++ src/components/NavMenu/NavMenu.component.js
@@ -2,12 +2,15 @@ import './styles.css';
 
 const NavMenu = () => {
   return (
-    <div className='menu-button'>
-      <div className='menu-line-container'>
-        <div className='menu-line' />
-        <div className='menu-line' />
-        <div className='menu-line' />
+    <div className='menu'>
+      <div className='menu-button'>
+        <div className='menu-line-container'>
+          <div className='menu-line' />
+          <div className='menu-line' />
+          <div className='menu-line' />
+        </div>
       </div>
+      <div className='menu-list'></div>
     </div>
   );
 };
