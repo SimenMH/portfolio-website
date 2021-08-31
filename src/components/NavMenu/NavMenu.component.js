f931726a4d6b1e1e3b03cfa4fb18458ec1f0d28b
Simen H <simenmelo@gmail.com>
2021-08-31 10:48:00 +0200

    feat: Populate menu list

--- src/components/NavMenu/NavMenu.component.js
+++ src/components/NavMenu/NavMenu.component.js
@@ -1,6 +1,22 @@
 import { useState } from 'react';
 import './styles.css';
 
+const menuItems = [
+  { name: 'Home', path: 'home', submenu: false },
+  { name: 'About me', path: 'about', submenu: false },
+  {
+    name: 'Porfolio',
+    path: 'portfolio',
+    submenu: [
+      { name: 'Web', path: 'portfolio/web' },
+      { name: 'Mobile', path: 'portfolio/mobile' },
+      { name: 'Games', path: 'portfolio/games' },
+    ],
+    toggled: false,
+  },
+  { name: 'Contact', path: 'contact', submenu: false },
+];
+
 const NavMenu = () => {
   const [showMenu, setShowMenu] = useState(false);
 
@@ -10,8 +26,34 @@ const NavMenu = () => {
     });
   };
 
+  const renderMenuItems = () => {
+    return menuItems.map((item, index, arr) => {
+      return (
+        <div className='menu-item-container'>
+          <div className='menu-item'>
+            <h2>{item.name}</h2>
+          </div>
+          {item.submenu &&
+            item.submenu.map((item, index, arr) => {
+              return (
+                <div className='menu-item submenu-item'>
+                  <div className='submenu-list-line' />
+                  <h2>
+                    <div className='submenu-dot' />
+                    {item.name}
+                  </h2>
+                </div>
+              );
+            })}
+          {index !== arr.length - 1 && <div className='menu-list-line' />}
+        </div>
+      );
+    });
+  };
+
   return (
     <div className={`menu ${showMenu ? 'menu-show' : ''}`}>
+      {/* Menu Button */}
       <div className='menu-button' onClick={toggleMenu}>
         <div className='menu-line-container'>
           <div className='menu-line' />
@@ -19,7 +61,8 @@ const NavMenu = () => {
           <div className='menu-line' />
         </div>
       </div>
-      <div className='menu-list'></div>
+      {/* Menu list */}
+      <div className='menu-list'>{renderMenuItems()}</div>
     </div>
   );
 };
