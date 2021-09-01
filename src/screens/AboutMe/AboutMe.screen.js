68bf9fe3857eee030ac15d35ede6a4b02dff247c
Simen H <simenmelo@gmail.com>
2021-09-01 06:50:00 +0200

    feat: Add About Me text content

--- src/screens/AboutMe/AboutMe.screen.js
+++ src/screens/AboutMe/AboutMe.screen.js
@@ -3,7 +3,38 @@ import './styles.css';
 const AboutMe = () => {
   return (
     <div className='aboutme-container'>
-      <h1>Coming Soon</h1>
+      <h1>Who Am I?</h1>
+      <p>
+        I began my programming journey in 2012, when I wrote my very first Hello
+        World script in Java.
+      </p>
+      <p>
+        Ever since I've been infatuated with technology and the creativity
+        involved with building projects.
+      </p>
+      <p>
+        For the next 9 years I would continue to explore my love for technology
+        and broadening my knowledge of programming. Creating everything from
+        ambitious video games to software and tools used to improve the life of
+        my friends and family.
+      </p>
+      <p>
+        At the start of 2020, with my plans of working in the restaurant
+        industry seemingly halted by the pandemic, I drowned myself in code,
+        with a new ambition to put the skills and passion I had for programming
+        to better use in a professional environment.
+      </p>
+      <p>
+        After nearly 2 grusome years of hard work, filling all gaps and
+        sementing the knowledge I had gathered over the past years of doing it
+        as a hobby, I took my first job as a full-stack web developer and never
+        looked back.
+      </p>
+      <p>
+        Outside of technology, I spend my time hiking, fishing, and camping
+        alongside my best bud Biscuit, enjoying the tranquility that Norwegian
+        nature has to offer.
+      </p>
     </div>
   );
 };
