7b4ff48fbec6c56dbb64630404a1514fdda75e85
Simen H <simenmelo@gmail.com>
2021-09-01 07:19:00 +0200

    feat: Temporary short about me version

--- src/screens/AboutMe/AboutMe.screen.js
+++ src/screens/AboutMe/AboutMe.screen.js
@@ -3,8 +3,18 @@ import './styles.css';
 const AboutMe = () => {
   return (
     <div className='aboutme-container'>
-      <h1>Who Am I?</h1>
-      <p>I'm full-stack developer located in the frozen wastes of Norway.</p>
+      <h1>The short version</h1>
+      <p>
+        Ambitious and optimistic Full-Stack developer with a "nothing is
+        impossible" attitude.
+      </p>
+      <p>
+        Programmed as a hobby for 9 years. It's still a hobby, now I just get
+        paid.
+      </p>
+      <p>Other passions include animals, nature, and food.</p>
+      <h1>The rambling version</h1>
+      <p>Full-stack developer located in the frozen wastes of Norway.</p>
       <p>
         I began my programming journey in 2012, when I wrote my very first Hello
         World script in Java.
@@ -26,10 +36,9 @@ const AboutMe = () => {
         to better use in a professional environment.
       </p>
       <p>
-        After nearly 2 grusome years of hard work, filling all gaps and
-        sementing the knowledge I had gathered over the past years of doing it
-        as a hobby, I took my first job as a full-stack web developer and never
-        looked back.
+        After 2 years of hard work, filling any gaps and sementing the knowledge
+        I had gathered over the past years of doing it as a hobby, I took my
+        first job as a full-stack web developer and never looked back.
       </p>
       <p>
         Outside of technology, I spend my time hiking, fishing, and camping
