8797574afb0c978bdf43bbd901d459a9409452c4
Simen H <simenmelo@gmail.com>
2021-09-01 16:51:46 +0200

    feat: Replace NavArrow div with SVG

--- src/components/NavArrow/NavArrow.component.js
+++ src/components/NavArrow/NavArrow.component.js
@@ -10,7 +10,11 @@ const NavArrow = props => {
       }`}
     >
       <p className='label'>{label}</p>
-      <div className='nav-arrow' />
+      <div className='nav-arrow'>
+        <svg height='25' width='50' className='arrow-svg'>
+          <polyline points='1,25 25,1 50,25 25,1' />
+        </svg>
+      </div>
     </div>
   );
 };
