487877af405ec8ed4c9a5d7fc61f79446c451ae8
Simen H <simenmelo@gmail.com>
2021-09-02 16:51:59 +0200

    feat: Create contact form

--- src/screens/Contact/Contact.screen.js
+++ src/screens/Contact/Contact.screen.js
@@ -2,8 +2,51 @@ import './styles.css';
 
 const Contact = () => {
   return (
-    <div className='aboutme-container fade-in'>
-      <h1>Coming Soon!</h1>
+    <div className='screen-container contact-container fade-in'>
+      <h1>Contact me</h1>
+      <p>
+        Whether you're interested in hiring me for your next ambitious project,
+        or just want to drop me an anonymous message, don't hesitate to use this
+        form!
+      </p>
+      <form className='contact-form'>
+        <ul>
+          <li className='half'>
+            <input
+              className='input-field'
+              placeholder='Name'
+              type='text'
+              required={true}
+            />
+            <div className='input-focus' />
+          </li>
+          <li className='half'>
+            <input
+              className='input-field'
+              placeholder='Email (Optional)'
+              type='email'
+            />
+            <div className='input-focus' />
+          </li>
+          <li>
+            <input className='input-field' placeholder='Subject' type='text' />
+            <div className='input-focus' />
+          </li>
+          <li>
+            <textarea
+              className='input-field'
+              placeholder='Message'
+              required={true}
+            />
+            <div className='input-focus' />
+          </li>
+          <li>
+            <div className='submit-button'>
+              <p>Send message</p>
+            </div>
+          </li>
+        </ul>
+      </form>
     </div>
   );
 };
