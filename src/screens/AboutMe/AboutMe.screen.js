039e73cd6faaafc1c8cdff629ad8a5dd893321f7
Simen H <simenmelo@gmail.com>
2021-09-06 07:59:48 +0200

    style: Update about me title and text

--- src/screens/AboutMe/AboutMe.screen.js
+++ src/screens/AboutMe/AboutMe.screen.js
@@ -3,39 +3,27 @@ import './styles.css';
 const AboutMe = () => {
   return (
     <div className='aboutme-container fade-in'>
-      <h1>Who am I?</h1>
-      <p>Full-stack developer located in the frozen wastes of Norway.</p>
-      <p>
-        I began my programming journey in 2012, when I wrote my very first Hello
-        World script in Java.
-      </p>
-      <p>
-        Ever since I've been infatuated with technology and the creativity
-        involved with building projects.
-      </p>
-      <p>
-        For the next 9 years I would continue to explore my love for technology
-        and broadening my knowledge of programming. Creating everything from
-        ambitious video games to software and tools used to improve the life of
-        my friends and family.
-      </p>
-      <p>
-        At the start of 2020, with my plans of working in the restaurant
-        industry seemingly halted by the pandemic, I drowned myself in code,
-        with a new ambition to put the skills and passion I had for programming
-        to better use in a professional environment.
-      </p>
-      <p>
-        After 2 years of hard work, filling any gaps and sementing the knowledge
-        I had gathered over the past years of doing it as a hobby, I took my
-        first job as a full-stack web developer and never looked back.
-      </p>
-      <p>
-        Outside of technology, I spend my time hiking, fishing, and camping
-        alongside my best bud Biscuit, enjoying the tranquility that Norwegian
-        nature has to offer.
-      </p>
-      <h1>Skills</h1>
+      <h1>A Little About Me</h1>
+      <div className='about-text'>
+        <p>
+          Full-stack developer located in Norway. I have been programming since
+          2012, and have a serious passion for building unique projects and
+          creating intuitive and dynamic user experiences.
+        </p>
+        <p>
+          Well-organized, positive, and highly motivated problem-solver with
+          great attention to detail. Other passions include cooking, video
+          games, outdoor activities and surrounding myself with animals.
+        </p>
+        <p>
+          Knowledgeable of both the back and frontend development spectrum.
+          Familiar with a wide range of programming utilities and languages, and
+          love working on ambitious and impactful projects with positive and
+          motivated people.
+        </p>
+      </div>
+
+      <h1>What I Work With</h1>
       <div className='skills-container'>
         <div className='skill'>
           <div className='skill-icon' />
