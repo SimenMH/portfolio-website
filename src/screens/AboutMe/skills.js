bb002a9cf87c80d350635ffa6c62a09293af0123
Simen H <simenmelo@gmail.com>
2021-09-06 14:16:01 +0200

    feat: Seperate skill list array from AboutMe component

---
+++ src/screens/AboutMe/skills.js
@@ -0,0 +1,134 @@
+import JavaScriptLogo from '../../assets/skill-logos/JavaScript-logo.svg';
+import HTML5Logo from '../../assets/skill-logos/HTML5-logo.svg';
+import CSS3Logo from '../../assets/skill-logos/CSS3-logo.svg';
+import ReactLogo from '../../assets/skill-logos/React-logo.svg';
+import AngularLogo from '../../assets/skill-logos/Angular-logo.svg';
+import SASSLogo from '../../assets/skill-logos/SASS-logo.svg';
+
+const skills = [
+  {
+    category: 'Frontend',
+    skills: [
+      {
+        name: 'JavaScript',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: JavaScriptLogo,
+      },
+      {
+        name: 'HTML5',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: HTML5Logo,
+      },
+      {
+        name: 'CSS3',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: CSS3Logo,
+      },
+      {
+        name: 'React',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: ReactLogo,
+      },
+      {
+        name: 'Angular',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: AngularLogo,
+      },
+      {
+        name: 'SASS',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: SASSLogo,
+      },
+    ],
+  },
+  {
+    category: 'Backend',
+    skills: [
+      {
+        name: 'NodeJS',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+      {
+        name: 'Express',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+      {
+        name: 'SQL',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+      {
+        name: 'GraphQL',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+      {
+        name: 'MongoDB',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+      {
+        name: 'REST APIs',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+    ],
+  },
+  {
+    category: 'Other',
+    skills: [
+      {
+        name: 'Python',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+      {
+        name: 'C#',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+      {
+        name: 'Git',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+      {
+        name: 'Docker',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+      {
+        name: 'Agile',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+      {
+        name: 'Game Development',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel enim varius, ullamcorper eros sed, condimentum eros.',
+        logo: 'path',
+      },
+    ],
+  },
+];
+
+export default skills;
