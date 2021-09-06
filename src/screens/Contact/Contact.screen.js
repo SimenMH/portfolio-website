baea490579d283bea0f508e4f0913a5a19817188
Simen H <simenmelo@gmail.com>
2021-09-06 09:59:05 +0200

    feat: Create envelope animation on submitting contact form

--- src/screens/Contact/Contact.screen.js
+++ src/screens/Contact/Contact.screen.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import './styles.css';
 
 const Contact = () => {
+  const [animate, setAnimate] = useState(false);
+
+  const sendMessage = () => {
+    setAnimate(true);
+  };
+
   return (
     <div className='screen-container contact-container fade-in'>
       <h1>Contact me</h1>
@@ -9,40 +16,77 @@ const Contact = () => {
         or just want to drop me an anonymous message, don't hesitate to use this
         form!
       </p>
-      <form className='contact-form'>
-        <div className='form-grid-item form-grid-half'>
-          <input
-            className='input-field'
-            placeholder='Name'
-            type='text'
-            required={true}
-          />
-          <label className='input-focus' />
+      <div>
+        <div className={`envelope ${animate ? 'animate-env' : ''}`}>
+          <div className={`envelope-left ${animate ? 'animate-left-env' : ''}`}>
+            <svg width='900' height='700'>
+              <polyline points='0,0 450,350 0,700' />
+            </svg>
+          </div>
+          <div
+            className={`envelope-right ${animate ? 'animate-right-env' : ''}`}
+          >
+            <svg width='900' height='700'>
+              <polyline points='900,0 450,350 900,700' />
+            </svg>
+          </div>
+          <div
+            className={`envelope-bottom ${animate ? 'animate-bottom-env' : ''}`}
+          >
+            <svg width='600' height='380'>
+              <polyline points='0,380 300,190 600,380' />
+            </svg>
+          </div>
+          <div className={`envelope-top ${animate ? 'animate-top-env' : ''}`}>
+            <svg width='600' height='250'>
+              <polyline points='0,0 300,190 600,0' />
+            </svg>
+          </div>
         </div>
-        <div className='form-grid-item form-grid-half'>
-          <input
-            className='input-field'
-            placeholder='Email (Optional)'
-            type='email'
-          />
-          <label className='input-focus' />
-        </div>
-        <div className='form-grid-item'>
-          <input className='input-field' placeholder='Subject' type='text' />
-          <label className='input-focus' />
-        </div>
-        <div className='form-grid-item'>
-          <textarea
-            className='input-field'
-            placeholder='Message'
-            required={true}
-          />
-          <label className='input-focus' />
-        </div>
-        <div className='form-grid-item submit-button'>
+
+        <form
+          className={`contact-form ${animate ? 'hide-form' : ''}`}
+          onAnimationEnd={() => setAnimate(false)}
+        >
+          <div className='form-grid-item form-grid-half'>
+            <input
+              className='input-field'
+              placeholder='Name'
+              type='text'
+              required={true}
+            />
+            <label className='input-focus' />
+          </div>
+          <div className='form-grid-item form-grid-half'>
+            <input
+              className='input-field'
+              placeholder='Email (Optional)'
+              type='email'
+            />
+            <label className='input-focus' />
+          </div>
+          <div className='form-grid-item'>
+            <input className='input-field' placeholder='Subject' type='text' />
+            <label className='input-focus' />
+          </div>
+          <div className='form-grid-item'>
+            <textarea
+              className='input-field'
+              placeholder='Message'
+              type='text'
+              required={true}
+            />
+            <label className='input-focus' />
+          </div>
+        </form>
+        <button
+          type='submit'
+          className='form-grid-item submit-button'
+          onClick={sendMessage}
+        >
           <p>Send message</p>
-        </div>
-      </form>
+        </button>
+      </div>
     </div>
   );
 };
