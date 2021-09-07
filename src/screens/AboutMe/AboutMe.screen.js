3edbbfbc35b789cb02a8dc9a4081671f0a718333
Simen H <simenmelo@gmail.com>
2021-09-07 09:13:19 +0200

    style: Add gradient to about me text

--- src/screens/AboutMe/AboutMe.screen.js
+++ src/screens/AboutMe/AboutMe.screen.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import skillList from './skills.js';
 
@@ -19,73 +20,80 @@ const AboutMe = () => {
 
   return (
     <div className='aboutme-container fade-in'>
-      <section className='aboutme-text-section'>
-        <h1>A Little About Me</h1>
-
-        <p>
-          Full-stack developer located in Norway. I have been programming since
-          2012, and have a serious passion for building unique projects and
-          creating intuitive and dynamic user experiences.
-        </p>
-        <p>
-          Well-organized, positive, and highly motivated problem-solver with
-          great attention to detail. Other passions include cooking, video
-          games, outdoor activities and surrounding myself with animals.
-        </p>
-        <p>
-          Knowledgeable of both the back and frontend development spectrum.
-          Familiar with a wide range of programming utilities and languages, and
-          love working on ambitious and impactful projects with positive and
-          motivated people.
-        </p>
-      </section>
-      <section className='aboutme-skills-section'>
-        <h2>
-          My Skills
-          <div className='faded-line-seperator' />
-        </h2>
-        <h3 className='skill-category'>{skillList[categoryIdx].category}</h3>
-        <div className='skills-container'>
-          <div
-            className='skills-cycle-button'
-            onClick={() => cycleCategory(-1)}
-          >
-            <div className='skills-cycle-arrow skills-arrow-left' />
-          </div>
-          <div className='skills-grid'>
-            {skillList[categoryIdx].skills.map(skill => {
-              return (
-                <div className='skill'>
-                  <div className='skill-icon'>
-                    <img
-                      src={skill.logo}
-                      alt={`${skill.name} logo`}
-                      style={{ width: '100%', height: '100%' }}
-                    />
+      <div className='content-container'>
+        <section className='aboutme-text-section'>
+          <h1>A Little About Me</h1>
+          <p>
+            Full-stack developer living in Norway. Been a programmer since 2012,
+            and have a serious passion for building unique projects and creating
+            intuitive and dynamic user experiences.
+          </p>
+          <p>
+            Well-organized, positive, and highly motivated problem-solver with
+            great attention to detail. Other passions include cooking, video
+            games, outdoor activities and surrounding myself with animals.
+          </p>
+          <p>
+            Knowledgeable of both the back and frontend development spectrum.
+            Familiar with a wide range of programming utilities and languages,
+            and love working on ambitious and impactful projects with positive
+            and motivated people.
+          </p>
+          <Link className='get-in-touch' to='/contact'>
+            Get in touch!
+          </Link>
+        </section>
+        <section className='aboutme-skills-section'>
+          <h2>
+            My Skills
+            <div className='faded-line-seperator' />
+          </h2>
+          <h3 className='skill-category'>{skillList[categoryIdx].category}</h3>
+          <div className='skills-container'>
+            <div
+              className='skills-cycle-button'
+              onClick={() => cycleCategory(-1)}
+            >
+              <div className='skills-cycle-arrow skills-arrow-left' />
+            </div>
+            <div className='skills-grid'>
+              {skillList[categoryIdx].skills.map(skill => {
+                return (
+                  <div className='skill'>
+                    <div className='skill-icon'>
+                      <img
+                        src={skill.logo}
+                        alt={`${skill.name} logo`}
+                        style={{ width: '100%', height: '100%' }}
+                      />
+                    </div>
+                    <h3 className='skill-name'>{skill.name}</h3>
+                    {/* <p>{skill.description}</p> */}
                   </div>
-                  <h3 className='skill-name'>{skill.name}</h3>
-                  {/* <p>{skill.description}</p> */}
-                </div>
+                );
+              })}
+            </div>
+            <div
+              className='skills-cycle-button'
+              onClick={() => cycleCategory(1)}
+            >
+              <div className='skills-cycle-arrow skills-arrow-right' />
+            </div>
+          </div>
+          <div className='category-dots-container'>
+            {skillList.map((_, idx) => {
+              return (
+                <div
+                  className={`category-dot ${
+                    idx === categoryIdx ? 'active-category-dot' : ''
+                  }`}
+                  onClick={() => setCategoryIdx(idx)}
+                />
               );
             })}
           </div>
-          <div className='skills-cycle-button' onClick={() => cycleCategory(1)}>
-            <div className='skills-cycle-arrow skills-arrow-right' />
-          </div>
-        </div>
-        <div className='category-dots-container'>
-          {skillList.map((_, idx) => {
-            return (
-              <div
-                className={`category-dot ${
-                  idx === categoryIdx ? 'active-category-dot' : ''
-                }`}
-                onClick={() => setCategoryIdx(idx)}
-              />
-            );
-          })}
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
