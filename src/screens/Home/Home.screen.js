1a2d8187f6848084831210268fcb5091450694a3
SimenMH <simenmelo@gmail.com>
2022-10-01 23:43:59 +0200

    add last name

--- src/screens/Home/Home.screen.js
+++ src/screens/Home/Home.screen.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className='home-container fade-in'>
       <div className='home-text'>
-        <h1>Hi, I'm Simen</h1>
+        <h1>Hi, I'm Simen Herland</h1>
         <h1>Software Engineer</h1>
         <div className='line-seperator' />
         <h2>A Full Stack / Front End web developer from Norway.</h2>
