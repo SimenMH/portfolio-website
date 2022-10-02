9869864a7c9b71f8361b60182469673cbb4d9482
SimenMH <simenmelo@gmail.com>
2022-10-02 11:35:13 +0200

    remove last name

--- src/screens/Home/Home.screen.js
+++ src/screens/Home/Home.screen.js
@@ -1,13 +1,13 @@
 import './styles.css';
 import SoMeLinks from '../../components/SoMeLinks/SoMeLinks.component';
 
-import CVPDF from '../../assets/Simen Herland - CV.pdf';
+// import CVPDF from '../../assets/Simen Herland - CV.pdf';
 
 const Home = () => {
   return (
     <div className='home-container fade-in'>
       <div className='home-text'>
-        <h1>Hi, I'm Simen Herland</h1>
+        <h1>Hi, I'm Simen</h1>
         <h1>Software Engineer</h1>
         <div className='line-seperator' />
         <h2>A Full Stack / Front End web developer from Norway.</h2>
