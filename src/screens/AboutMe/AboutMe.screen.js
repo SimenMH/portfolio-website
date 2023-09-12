f8c4d576689e2fff86b3f249e1855b143fe9cc92
SimenMH <simenmelo@gmail.com>
2023-09-12 12:27:55 +0200

    update About Me text

--- src/screens/AboutMe/AboutMe.screen.js
+++ src/screens/AboutMe/AboutMe.screen.js
@@ -9,34 +9,32 @@ const AboutMe = () => {
         <div className='aboutme-container__text'>
           <h1>About Me</h1>
           <p>
-            As a full-stack developer based in Norway, I have been passionately
-            immersed in the world of programming since 2012. I take immense
-            pride in crafting exceptional projects and designing intuitive user
-            experiences.
+            Hi, I'm Simen. A full-stack developer based in Norway who has had a
+            serious passion for programming since 2012.
           </p>
           <p>
-            Organized, positive, and highly motivated, I thrive on solving
-            problems with meticulous attention to detail. I find immense
-            satisfaction in creating seamless solutions that exceed
-            expectations.
+            I take immense pride and enjoyment in building exceptional projects
+            alongside exceptional people. I consider myself organized, positive,
+            and highly motivated. I thrive on solving problems with an
+            undoubtedly great attention to detail.
           </p>
           <p>
-            When I'm not immersed in coding, I find solace in exploring my other
+            When I'm not immersed in work, I find solace in exploring my other
             passions, such as cooking, engaging in outdoor activities, and
             embracing the company of animals. These passions not only bring
             balance to my life but also inspire creativity in my work.
           </p>
           <p>
             My expertise extends across both front-end and back-end development,
-            allowing me to navigate the entire development spectrum. I am
-            well-versed in various programming languages and utilities, allowing
-            me to take on ambitious projects alongside like-minded individuals
-            who share the same strong drive.
+            and I enjoy working on the entire development spectrum. I am
+            well-versed in various programming languages and utilities, which
+            makes me well prepared to take on ambitious projects alongside
+            like-minded individuals who share the same strong drive.
           </p>
           <p>
-            If you share a passion for innovative endeavors and are seeking a
-            dedicated full-stack developer, I would be thrilled to collaborate
-            with you.
+            If you share a passion for innovative endeavors and are looking for
+            a dedicated full-stack developer, I would be thrilled to collaborate
+            with you!
           </p>
           <Link className='aboutme-container__get-in-touch' to='/contact'>
             Get in touch!
